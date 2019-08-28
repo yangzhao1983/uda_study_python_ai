b555645fd0997dd6ccaafffdad88c857e11b4430
third-eye <kaiser_zhao1983@sohu.com>
2019-08-28 16:41:51 +0800

    turtle python 02

---
+++ hello_world_turtle.py
@@ -0,0 +1,10 @@
+import turtle
+import tkinter
+fred = turtle.Turtle()
+fred.color("red")
+fred.forward(100)
+fred.right(135)
+fred.forward(140)
+fred.right(135)
+fred.forward(100)
+tkinter.mainloop()
